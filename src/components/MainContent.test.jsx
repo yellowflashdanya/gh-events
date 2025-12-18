b14b04907416efad17232103fa506645b7d6901c
Danylo Melnychuk <danyakid2005@gmail.com>
2025-12-18 03:27:21 +0200

    added comment

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
-// My tests
+// My tests added
 
 describe('MainContent', () => {
   it('should render a button', () => {
